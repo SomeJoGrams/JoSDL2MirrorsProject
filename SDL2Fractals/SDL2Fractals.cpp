2f165d2bc650f42874d8c2c9a84954bff9ea8c93
SomeJoGrams <jonathan.liening@t-online.de>
2023-05-08 17:48:08 +0200

    Projektdateien hinzufügen.

---
+++ SDL2Fractals/SDL2Fractals.cpp
@@ -0,0 +1,38 @@
+﻿// SDL2BaseVCPKG.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "SDL2BaseVCPKG.h"
+
+
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "My SDL2 Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == NULL)
+    {
+        printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    // ...
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
---
+++ SDL2Fractals/SDL2Fractals.h
@@ -0,0 +1,10 @@
+﻿// SDL2BaseVCPKG.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+#define SDL_MAIN_HANDLED // alternative for linking with visual studio
+#include "SDL2/SDL.h"
+
+// TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
