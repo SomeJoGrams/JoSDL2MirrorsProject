998bc0f5ee7aef220332867e392fd8124580f347
SomeJoGrams <jonathan.liening@t-online.de>
2023-05-16 12:03:35 +0200

    added cmake for testing + preset

--- SDL2Mirrors/Testing/TestingReflections.cpp
+++ SDL2Mirrors/Testing/TestingReflections.cpp
@@ -1,7 +1,63 @@
 
-#include "gtest.h"
+#include "gtest/gtest.h"
 
 
-int main(){
-return 0;
+
+#include "BorderHit.hpp"
+
+
+namespace BorderHit {
+
+	TEST(BorderHitPositionTest, PosEqualOperation) {
+		BorderHit::Position2D firstPos{ 1,1 };
+		BorderHit::Position2D secondPos{ 1,1 };
+		EXPECT_EQ(firstPos, secondPos);
+	}
+
+
+    class HitLineTest : public testing::Test {
+    protected:
+        // You can remove any or all of the following functions if their bodies would
+        // be empty.
+
+        HitLineTest() {
+            // You can do set-up work for each test here.
+        }
+
+        ~HitLineTest() override {
+            // You can do clean-up work that doesn't throw exceptions here.
+        }
+
+        // If the constructor and destructor are not enough for setting up
+        // and cleaning up each test, you can define the following methods:
+
+        void SetUp() override {
+            // Code here will be called immediately after the constructor (right
+            // before each test).f
+
+        }
+
+        void TearDown() override {
+            // Code here will be called immediately after each test (right
+            // before the destructor).
+        }
+
+        // Class members declared here can be used by all tests in the test suite
+        // for Foo.
+    };
+
+    // test for a single origin
+    TEST_F(HitLineTest, AngleCalculation) {
+        EXPECT_EQ(1, 1);
+    }
+
+    // TODO use the fixture
+
+}
+
+}
+
+int main(int argc, char** argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
--- SDL2Mirrors/include/Helper.hpp
+++ SDL2Mirrors/include/Helper.hpp
@@ -1,4 +1,4 @@
-
+#pragma once
 #include <cmath> // for remainder operation
 
 #pragma once
