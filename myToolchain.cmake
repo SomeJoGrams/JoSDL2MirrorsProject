3de3a5764cce6fd557ef87cbe79a25f63f5a1088
SomeJoGrams <jonathan.liening@t-online.de>
2023-05-30 16:03:08 +0200

    called the correct submodule command, removed emscripten sdk for the moment

--- myToolchain.cmake
+++ myToolchain.cmake
@@ -5,5 +5,5 @@
 include("${CMAKE_CURRENT_SOURCE_DIR}/SDL2Mirrors/extern/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 #for emscripten
-include("H:/FastDocuments/Bibliotheken/ProgrammierBibliotheken/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+#include("H:/FastDocuments/Bibliotheken/ProgrammierBibliotheken/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
