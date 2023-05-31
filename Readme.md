c6bb64a6917d657f95e07c01eefffb242594a722
SomeJoGrams <jonathan.liening@t-online.de>
2023-05-31 19:16:53 +0200

    add emscripten workflow

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 [![CMake](https://github.com/SomeJoGrams/JoSDL2MirrorsProject/actions/workflows/cmake.yml/badge.svg)](https://github.com/SomeJoGrams/JoSDL2MirrorsProject/actions/workflows/cmake.yml)
+
+[![Emscripten](https://github.com/SomeJoGrams/JoSDL2MirrorsProject/actions/workflows/emscripten.yml/badge.svg)](https://github.com/SomeJoGrams/JoSDL2MirrorsProject/actions/workflows/emscripten.yml)
--- myToolchain.cmake
+++ myToolchain.cmake
@@ -5,5 +5,6 @@
 include("${CMAKE_CURRENT_SOURCE_DIR}/SDL2Mirrors/extern/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 #for emscripten
-#include("H:/FastDocuments/Bibliotheken/ProgrammierBibliotheken/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+#from the github action currently expected path
+include("${CMAKE_CURRENT_SOURCE_DIR}/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
 
